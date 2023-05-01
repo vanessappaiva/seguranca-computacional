6c14d58764068a597169cbc8374fac7a671f61be
Vanessa Paiva <vanespaiva@gmail.com>
2023-05-01 10:04:00 -0300

    First commit

---
+++ partei.py
@@ -0,0 +1,7 @@
+# Curso: Ciências da Computação - CIC/UNB
+# Aluna: Vanessa Paz Araújo Paiva
+# Matrícula: 180091905
+# E-mail: vanespaiva@gmail.com
+# Disciplina: CIC0201 - Segurança Computacional - T02
+# Professor: Joao Jose Costa Gondim
+# Data: 01/05/2023
