4c47426a8f17e51d3b1645daac732f5eb7034ebc
Vanessa Paiva <77133785+vanessappaiva@users.noreply.github.com>
2024-02-05 15:01:59 -0300

    Update partei.py

--- Trabalho 1 - CIC0201 - Vanessa Paiva/partei.py
+++ Trabalho 1 - CIC0201 - Vanessa Paiva/partei.py
@@ -1,7 +1,7 @@
 # Curso: Ciências da Computação - CIC/UNB
 # Aluna: Vanessa Paz Araújo Paiva
-# Matrícula: 180091905
-# E-mail: vanespaiva@gmail.com
+# Matrícula: ***
+# E-mail: ***
 # Disciplina: CIC0201 - Segurança Computacional - T02
-# Professor: Joao Jose Costa Gondim
+# Professor: Joao J****
 # Data: 01/05/2023
